226afb01c34d5fecde98932cb32a27afe4828110
ShivRaajPatil <raajpatil0909@gmail.com>
2022-08-04 20:49:20 +0530

    Added code for brand webpage

---
+++ src/test/java/regression/NewClassByShivraj.java
@@ -0,0 +1,8 @@
+package regression;
+
+public class NewClassByShivraj {
+	public static void main(String[] args) {
+		System.out.println("KTCTC");
+	}
+
+}
--- src/test/java/regression/SampleClass.java
+++ src/test/java/regression/SampleClass.java
@@ -4,6 +4,7 @@ public class SampleClass {
 
 	public static void main(String[] args) {
 		System.out.println("Git practice");
+		System.out.println(" Code updated by Shivraj");
 		
 		
 
